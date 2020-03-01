82887cd2d1396a31af0b08ce70fdbd90aeaf3ef7
Santiago Bandiera <sagaban@hotmail.com>
2020-03-01 18:11:24 -0300

    Add ch02 tuples

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@ mod chapters;
 
 use chapters::{c01, c02};
 
+fn separator() {
+    println!("\n \n ---------------------------------------- \n \n")
+}
 fn main() {
-    // c01::format_string();
-    // c01::display();
-    // c01::testcase_list();
-    // c01::formatting();
+    c01::format_string();
+    separator();
+    c01::display();
+    separator();
+    c01::testcase_list();
+    separator();
+    c01::formatting();
+    separator();
     c02::primitives_and_operations();
-
-
+    separator();
+    c02::tuples();
 }
