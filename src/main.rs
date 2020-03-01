88fc6f58f71cae2eda1804c62bf2ef5a2cc7ff0e
Santiago Bandiera <sagaban@hotmail.com>
2020-03-01 17:54:26 -0300

    Add chapter 2 primitives and operators

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod chapters;
 
-use chapters::{c01, c02::chapter_main as chapter_02};
+use chapters::{c01, c02};
 
 fn main() {
     // c01::format_string();
     // c01::display();
     // c01::testcase_list();
-    c01::formatting();
+    // c01::formatting();
+    c02::primitives_and_operations();
 
 
-    chapter_02();
 }
